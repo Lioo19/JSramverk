f928c76f9d5811cc793f9693d6020b8f17f99417
Linnea <linnea.j.m.olofsson@gmail.com>
2020-09-23 16:01:45 +0200

    test for travis

--- test/selenium/test.js
+++ test/selenium/test.js
@@ -7,61 +7,61 @@ const By = webdriver.By;
 
 let browser;
 
+function goToNavLink(target) {
+    browser.findElement(By.linkText(target)).then(function(element) {
+        element.click();
+    });
+}
+
+function matchUrl(target) {
+    browser.getCurrentUrl().then(function(url) {
+        assert.ok(url.endsWith(target));
+    });
+}
+
+function assertH2(target) {
+    browser.findElement(By.css("h2")).then(function(element) {
+        element.getText().then(function(text) {
+            assert.equal(text, target);
+        });
+    });
+}
+
 // Test suite
 test.describe("Testing me-page", function() {
-    const baseURL = "https://me.linneaolofsson.me/";
-
     test.beforeEach(function(done) {
         this.timeout(50000);
         browser = new webdriver.Builder().
-            withCapabilities(webdriver.Capabilities.firefox()).build();
+            withCapabilities(webdriver.Capabilities.firefox())
+            .build();
 
         browser.get("https://me.linneaolofsson.me/");
         done();
     });
+
+    const baseURL = "https://me.linneaolofsson.me/";
+
 
     test.afterEach(function(done) {
         browser.quit();
         done();
     });
 
-
-    function goToNavLink(target) {
-        browser.findElement(By.linkText(target)).then(function(element) {
-            element.click();
-        });
-    }
-
-    function matchUrl(target) {
-        browser.getCurrentUrl().then(function(url) {
-            assert.ok(url.endsWith(target));
-        });
-    }
-
-    function assertH2(target) {
-        browser.findElement(By.css("h2")).then(function(element) {
-            element.getText().then(function(text) {
-                assert.equal(text, target);
-            });
-        });
-    }
-
     // Test case for index page, checking title, that the headline exists and that url is correct
     test.it("Test index", function(done) {
-        let promise = browser.getTitle();
+        // let promise = browser.getTitle();
 
-        promise.then(function(title) {
-            assert.equal(title, "React App");
-        });
+        // promise.then(function(title) {
+        //     assert.equal(title, "React App");
+        // });
 
-        browser.getTitle().then(function(title) {
-            assert.equal(title, "React App");
-        });
-
-        assertH2("ME-SIDA FÖR JSRAMVERK");
-        matchUrl(baseURL);
-
-        done();
+        browser.getTitle()
+            .then(function(title) {
+                assert.equal(title, "React App");
+        }).then(function() {
+            assertH2("ME-SIDA FÖR JSRAMVERK");
+            matchUrl(baseURL);
+        }).then(() => done());
     });
 
 
